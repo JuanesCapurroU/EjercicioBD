bb006de19fcd7e9cf08bc62401ae0d6643aebbac
Juan Capurro <juan.capurro@uniminuto.edu.co>
2024-09-06 17:05:35 -0500

    Calculo seno y coseno

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner teclado = new Scanner(System.in);
-    double numero1, numero2, suma, resta, multi, raiz1,raiz2;
+    double numero1, numero2, suma, resta, multi, raiz1,raiz2,seno,coseno;
 
         System.out.println("Ingrese el primer valor");
         numero1= teclado.nextDouble();
@@ -17,12 +17,16 @@ public class Main {
         multi= numero1*numero2;
         raiz1 = Math.sqrt(numero1);
         raiz2 = Math.sqrt(numero2);
+        seno = Math.sin(numero1);
+        coseno = Math.cos(numero2);
 
         System.out.println("La suma es "+suma);
         System.out.println("La resta es: "+ resta);
         System.out.println("La multiplicacion es: "+multi);
         System.out.println("La raiz 1 es: "+ raiz1);
         System.out.println("La raiz 2 es: "+ raiz2);
+        System.out.println("El seno es: "+ seno);
+        System.out.println("El coseno es: "+coseno);
 
     }
 }
