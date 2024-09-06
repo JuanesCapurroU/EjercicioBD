50463cfe864e62bb1ba5a8a7d16ebaea5edf082a
Juan Capurro <juan.capurro@uniminuto.edu.co>
2024-09-06 16:49:37 -0500

    Calculo de raiz

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner teclado = new Scanner(System.in);
-    double numero1, numero2, suma, resta, multi;
+    double numero1, numero2, suma, resta, multi, raiz1,raiz2;
 
         System.out.println("Ingrese el primer valor");
         numero1= teclado.nextDouble();
@@ -15,10 +15,14 @@ public class Main {
         suma = numero1+numero2;
         resta = numero1-numero2;
         multi= numero1*numero2;
+        raiz1 = Math.sqrt(numero1);
+        raiz2 = Math.sqrt(numero2);
 
         System.out.println("La suma es "+suma);
         System.out.println("La resta es: "+ resta);
         System.out.println("La multiplicacion es: "+multi);
+        System.out.println("La raiz 1 es: "+ raiz1);
+        System.out.println("La raiz 2 es: "+ raiz2);
 
     }
 }
